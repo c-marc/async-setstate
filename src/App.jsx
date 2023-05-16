f33809ac71e388967740bcd3bee656047402615a
Marc C <mcar176@gmail.com>
2023-05-16 17:42:01 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useRef, useEffect } from "react";
+
+function Test() {
+  const [disabled, setDisabled] = useState(false);
+  const myRef = useRef();
+
+  console.log(
+    "components says state disabled=",
+    disabled,
+    "and ref disabled=",
+    myRef.current?.disabled
+  );
+
+  useEffect(() => {
+    console.log(
+      "useEffect says state disabled=",
+      disabled,
+      "and ref disabled=",
+      myRef.current?.disabled
+    );
+  }, [disabled]);
+
+  useEffect(() => {
+    myRef.current.addEventListener("click", () =>
+      console.log("ref says click")
+    );
+  }, []);
+
+  const handleClick = async () => {
+    const something = async () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          console.log("Promise resolved");
+          resolve(true);
+        }, 5000);
+      });
+    };
+    console.log("handler says click");
+    setDisabled(true);
+    console.log("handler asked for state disabled true");
+    console.log(
+      "handler says state disabled=",
+      disabled,
+      "and ref disabled=",
+      myRef.current.disabled
+    );
+    console.log("handler says await");
+    await something();
+    console.log("handler says await resolved");
+    console.log(
+      "handler says state disabled=",
+      disabled,
+      "and ref disabled=",
+      myRef.current.disabled
+    );
+  };
+
+  return (
+    <button ref={myRef} disabled={disabled} onClick={handleClick}>
+      Fake
+    </button>
+  );
+}
+
+function App() {
+  return <Test />;
+}
+
+export default App;
